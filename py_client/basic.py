0d2e01cbdb9c5f2fb5e24d5adde0ca1b6b05de6c
mrcruz117 <mrcruz1989@gmail.com>
2022-11-14 16:49:07 -0500

    client made first request

---
+++ py_client/basic.py
@@ -0,0 +1,9 @@
+import requests
+
+
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/"
+
+get_res = requests.get(endpoint)
+print(get_res.text)
+print(get_res.status_code)
