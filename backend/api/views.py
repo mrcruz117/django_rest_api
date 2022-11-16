2ed2e06e804f2036ee056666d632a5201375cde2
mrcruz117 <mrcruz1989@gmail.com>
2022-11-16 10:24:46 -0500

    converting req to JSON dict

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,16 @@
+import json
 from django.http import JsonResponse
 
 
 def api_home(request, *args, **kwargs):
+    # request.body -> HttpRequest -> Django
     res = {"message": "Hello, this is your API response!"}
+
+    body = request.body  # byte string of JSON data
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    print(data)
     return JsonResponse(res)
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,6 +4,10 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/"
 
-get_res = requests.get(endpoint)
-print(get_res.text)
+
+get_res = requests.get(endpoint, params={"abc": 123}, json={
+                       "query": "hello world"})
+# HTTP REQ
+
+print(get_res.json())
 print(get_res.status_code)
