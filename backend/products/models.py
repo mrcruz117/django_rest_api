dd36fbb4ba26f7c8cf5ae4c969b412018a666e49
mrcruz117 <mrcruz1989@gmail.com>
2022-11-16 17:26:52 -0500

    added note Django Rest Framework Model Serializers next

--- backend/products/models.py
+++ backend/products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-
+# NEXT STEP: Django Rest Framework Model Serializers
 class Product(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField(blank=True, null=True)
